98c93d66028cedd70ff6229d723a838dd300a016
georgesilv3 <george.silv3@gmail.com>
2025-05-02 17:09:38 -0400

    initalizing the project

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route("/chat", methods=["GET"])
+def show_template():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    socketio.run(app)
+
